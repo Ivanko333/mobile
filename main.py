eb578f89e442e1134dbe7d1e5c780afb42242129
Your a <a@example.com>
2023-10-24 20:56:26 +0300

    vcxb

--- main.py
+++ main.py
@@ -14,16 +14,26 @@ class FirstScreen(Screen):
         layout1 = BoxLayout(padding=8, spacing=8, orientation='vertical')
         layout2 = BoxLayout(size_hint=(.5, None), pos_hint={'center_x': 0.5, 'center_y': 0.5}, height="30sp")
         layout3 = BoxLayout(size_hint=(.5, None), pos_hint={'center_x': 0.5, 'center_y': 0.5}, height="30sp")
-        input1 = TextInput(hint_text=instr.txt_name)
-        input2 = TextInput(hint_text="Введите Ваш возраст")
+        input1 = TextInput(hint_text="Введите имя")
+        self.input2 = TextInput(hint_text="Введите Ваш возраст")
         layout1.add_widget(text)
         layout2.add_widget(input1)
-        layout3.add_widget(input2)
+        layout3.add_widget(self.input2)
         layout1.add_widget(layout2)
         layout1.add_widget(layout3)
-        button = ScreenButton(self, text='Далее!!!', direction='left', goal='second', size_hint=(.3, .3), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        button = Button(text='Далее!!!', size_hint=(.3, .3),
+                        pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        button.on_press = self.next
         layout1.add_widget(button)
         self.add_widget(layout1)
+
+    def next(self):
+        age = check_int(self.input2.text)
+        if not age or age < 7:
+            self.input2.text = "Ошибка! Введите целое число от 7!"
+
+        else:
+            self.manager.current = 'second'
 
 
 class MyApp(App):
@@ -41,29 +51,36 @@ class SecondScreen(Screen):
         super().__init__(**kwargs)
         text = Label(text=instr.txt_test1)
         text1 = Label(text="Введите результат:")
-        input1 = TextInput()
+        self.input1 = TextInput()
         layout = BoxLayout(padding=10, spacing=10, orientation='vertical')
         layout1 = BoxLayout(orientation='horizontal', size_hint=(1, None), height="30sp")
         layout.add_widget(text)
         layout1.add_widget(text1)
-        layout1.add_widget(input1)
+        layout1.add_widget(self.input1)
         layout.add_widget(layout1)
-        button = ScreenButton(self, text='Далее', direction='left', goal='third', size_hint=(.5, .2), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        button = Button(text='Далее', size_hint=(.5, .2),
+                        pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        button.on_press = self.next
         layout.add_widget(button)
         self.add_widget(layout)
+
+    def next(self):
+        result = check_int(self.input1.text)
+        if not result:
+            self.input1.text = "Ошибка! Введите целое число!"
+
+        else:
+            self.manager.current = 'third'
 
 
 class ThirdScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        text = Label(text=instr.txt_test3)
-        text1 = Label(text="Результат:")
-        text2 = Label(text='Результат после отдыха:')
-        input1 = TextInput(hint_text='0')
-        input2 = TextInput(hint_text='0')
+        text = Label(text=instr.txt_test2)
         layout1 = BoxLayout(padding=8, spacing=8, orientation='vertical')
         layout1.add_widget(text)
-        button = ScreenButton(self, text='Далее!!!', direction='left', goal='four', size_hint=(.3, .2), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        button = ScreenButton(self, text='Далее!!!', direction='left', goal='four', size_hint=(.3, .2),
+                              pos_hint={'center_x': 0.5, 'center_y': 0.5})
         layout1.add_widget(button)
         self.add_widget(layout1)
 
@@ -71,12 +88,37 @@ class ThirdScreen(Screen):
 class FourthScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        text = Label(text=instr.txt_test2)
+        text = Label(text=instr.txt_test3)
+        text1 = Label(text="Результате:")
+        text2 = Label(text="Результате после отдыха:")
+        self.input1 = TextInput(hint_text="0")
+        self.input2 = TextInput(hint_text="0")
         layout1 = BoxLayout(padding=8, spacing=8, orientation='vertical')
+        layout2 = BoxLayout(orientation="horizontal", size_hint=(1, None), height="30sp")
+        layout3 = BoxLayout(orientation="horizontal", size_hint=(1, None), height="30sp")
         layout1.add_widget(text)
-        button = ScreenButton(self, text='Далее!!!', direction='left', goal='main', size_hint=(.3, .2), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        layout2.add_widget(text1)
+        layout2.add_widget(self.input1)
+        layout3.add_widget(text2)
+        layout3.add_widget(self.input2)
+        layout1.add_widget(layout2)
+        layout1.add_widget(layout3)
+        button = Button(text='Далее!!!', size_hint=(.3, .2),
+                        pos_hint={'center_x': 0.5, 'center_y': 0.5})
         layout1.add_widget(button)
+        button.on_press = self.next
         self.add_widget(layout1)
+
+    def next(self):
+        result1 = check_int(self.input1.text)
+        result2 = check_int(self.input2.text)
+        if not result1:
+            self.input1.text = "Ошибка! Введите целое число!"
+        else:
+            if not result2:
+                self.input2.text = "Ошибка! Введите целое число!"
+            else:
+                self.manager.current = 'main'
 
 
 class ScreenButton(Button):
@@ -89,6 +131,15 @@ class ScreenButton(Button):
     def on_press(self):
         self.screen.manager.transition.direction = self.direction
         self.screen.manager.current = self.goal
+
+
+def check_int(number):
+    try:
+        number = int(number)
+        return number
+    except Exception as err:
+        print(err)
+        return False
 
 
 application = MyApp()
